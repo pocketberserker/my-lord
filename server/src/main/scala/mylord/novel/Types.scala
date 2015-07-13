a22c7a037fe70c0d68ebcd2c62ffa0aa8a02238e
pocketberserker <pocketberserker@gmail.com>
2015-07-13 17:55:44 +0900

    シナリオの型準備

---
+++ server/src/main/scala/mylord/novel/Types.scala
@@ -0,0 +1,15 @@
+package mylord.novel
+
+abstract class Novel(title: String)
+
+case class Scenario(title: String, background: String, scene: List[Scene]) extends Novel(title)
+
+abstract class Scene(background: String, characters: List[Character])
+
+case class Monologue(background: String, characters: List[Character], words: List[String])
+  extends Scene(background, characters)
+
+case class Line(background: String, characters: List[Character], words: List[String], name: String)
+  extends Scene(background, characters)
+
+case class Character(image: String, width: Int, height: Int)
